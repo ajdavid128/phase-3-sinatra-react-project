944bb2f3cb0bd4f4ce37aae6c0ce796ef93960ae
Leah Cardoz <leahcardoz@gmail.com>
2022-12-05 17:33:46 -0500

    added some seeds, testing image of johnny d

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+johnny_d = Artist.create(name: "Johnny Dombrowski", location: "New York", bio: "Comic-style illustrator", artist_photo: "", website_url: "http://www.johnnydombrowski.com/")
+Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
+Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
+Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
+Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
+
+Print.create(title: "Phish Halloween Gig Poster", year: 2021, edition_size: 1500, layers: 4, category: "concert", sub_category: "", image_url: "", artist_id: johnny_d.id)
+Print.create(title: "", year: , edition_size: , layers: , category: "", sub_category: "", image_url: "", artist_id: )
+Print.create(title: "", year: , edition_size: , layers: , category: "", sub_category: "", image_url: "", artist_id: )
+Print.create(title: "", year: , edition_size: , layers: , category: "", sub_category: "", image_url: "", artist_id: )
+Print.create(title: "", year: , edition_size: , layers: , category: "", sub_category: "", image_url: "", artist_id: )
 
 puts "✅ Done seeding!"

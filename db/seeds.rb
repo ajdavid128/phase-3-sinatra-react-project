962274433d34b41d167ea17484ad62b1c30228d7
Aaron David <ajdavid128@gmail.com>
2022-12-06 11:16:07 -0600

    Aaron added all of his seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,43 @@ Print.create(title: "Shaun Of The Dead", year: 2019, edition_size: "300", catego
 
 drew_m = Artist.create(name: "Drew Millward", location: "Leeds, England", bio: "Drew likes to draw ‘the old fashioned way’, using pencils, pens and a love of the craft of illustration, but is incresingly reliant on digital methods to create his work. While his subject matter can vary dramatically, he hopes that the attention to detail and the love of drawing is apparent throughout his work.", artist_photo: "", website_url: "http://www.drewmillward.portfoliobox.me/")
 # NEED TO ADD: drew_m artist_photo
-Print.create(title: "Umphrey's McGee Gig Poster", year: 2022, edition_size: "850", category: "concert", sub_category: "", image_url: "https://farm66.staticflickr.com/65535/52179507979_21fa098f65_z.jpg", artist_id: drew_m)
-Print.create(title: "Death Party", year: 2020, edition_size: "100", category: "art", sub_category: "social issues", image_url: "https://printsofthieves.co.uk/shop/wp-content/uploads/2022/10/deathfinal.webp", artist_id: drew_m)
-Print.create(title: "The Creep Parade 2", year: 2022, edition_size: "120", category: "art", sub_category: "", image_url: "https://printsofthieves.co.uk/shop/wp-content/uploads/2022/04/Creep-Parade-2.jpg", artist_id: drew_m)
+Print.create(title: "Umphrey's McGee Gig Poster", year: 2022, edition_size: "850", category: "concert", sub_category: "", image_url: "https://farm66.staticflickr.com/65535/52179507979_21fa098f65_z.jpg", artist_id: drew_m.id)
+Print.create(title: "Death Party", year: 2020, edition_size: "100", category: "art", sub_category: "social issues", image_url: "https://printsofthieves.co.uk/shop/wp-content/uploads/2022/10/deathfinal.webp", artist_id: drew_m.id)
+Print.create(title: "The Creep Parade 2", year: 2022, edition_size: "120", category: "art", sub_category: "", image_url: "https://printsofthieves.co.uk/shop/wp-content/uploads/2022/04/Creep-Parade-2.jpg", artist_id: drew_m.id)
 
 faith_s = Artist.create(name: "Faith Sponsler (Augury Press)", location: "Portland, OR", bio: "Faith is mainly a block printer, and her prints often focus on astrology, song lyrics, cats, Danny DeVito and other fun things", artist_photo: "https://i.etsystatic.com/15947738/c/1322/1050/678/265/il/4ac670/3924726158/il_570xN.3924726158_iyl2.jpg", website_url: "https://www.augurypress.com/")
 Print.create(title: "Mushroom Cowboy", year: 2022, edition_size: "open", category: "art", sub_category: "block", image_url: "https://i.etsystatic.com/15947738/r/il/a219c0/4335341010/il_fullxfull.4335341010_805p.jpg", artist_id: faith_s.id)
 Print.create(title: "Danny DeVito", year: , edition_size: "", category: "", sub_category: "", image_url: "https://www.augurypress.com/listing/1021611893/danny-devito-print-always-sunny-always", artist_id: faith_s.id)
 Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: faith_s.id)
 
-Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
+daniel_d = Artist.create(name: "Daniel Danger", location: "Massachusetts", bio: "Daniel Danger is an illustrator and print-maker living in the woods of Massachusetts. The son of a middle school art teacher and a professional potter, he was probably never going to be an accountant. His explanation of his own work is borderline nonsense and involves overlapping timelines, references to SIERRA point n' click adventure games, number stations in the desert oscillating whispered converations, the death of ghosts at the hands of foliage, giant trauma shadow beings born of kidnapped children that drip wolf shadows, and 1920s childrens books.", artist_photo: "https://images.squarespace-cdn.com/content/v1/537686bee4b065b57c0fc226/1524084510506-U39TD9FWSXH8IJZFZZSF/dangerfaceblue.jpg?format=1000w", website_url: "http://www.tinymediaempire.com/")
+Print.create(title: "If We Go, We Go Together", year: 2019, edition_size: "", category: "fine art print", sub_category: "", image_url: "https://images.squarespace-cdn.com/content/v1/537686bee4b065b57c0fc226/1558442451440-A22GR6S3R027T8VDF6CH/wegotogethersite.jpg?format=2500w", artist_id: daniel_d.id)
+Print.create(title: "We Can No Longer Protect You Forever", year: 2020, edition_size: "", category: "fine art print", sub_category: "", image_url: "https://images.squarespace-cdn.com/content/v1/537686bee4b065b57c0fc226/1585231129874-1DSAHX603MNTI2X401HB/pretectyouredux.jpg?format=2500w", artist_id: daniel_d.id)
+Print.create(title: "The Black Keys", year: 2019, edition_size: "", category: "concert poster", sub_category: "", image_url: "https://images.squarespace-cdn.com/content/v1/537686bee4b065b57c0fc226/1647871249094-JZA6RYMJQDGNWO9QI6L0/BlackKeysGIG-FINAL.jpg?format=2500w", artist_id: daniel_d.id)
 
-Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
+kevin_t = Artist.create(name: "Kevin Tong", location: "Austin, TX", bio: "Kevin Tong learned to draw in the competitive world of underground illustration, where the the best jobs were won in the knife fighting pits. Nevertheless, he prevailed and from that wood chipper furnace life his savage illustration style was forged. Since taking it to the streets, Kevin has been steadily illustrating since 2005. He currently lives and works in Austin, TX.", artist_photo: "http://www.bscgdc.com/blog/wp-content/uploads/2014/04/kevintong.jpg", website_url: "https://www.tragicsunshine.com/")
+Print.create(title: "Collisions", year: 2017, edition_size: "120", category: "fine art print", sub_category: "", image_url: "https://cdn.shopify.com/s/files/1/1108/5520/products/Deer.jpg?v=1510694030", artist_id: kevin_t.id)
+Print.create(title: "The Last of Us", year: 2016, edition_size: "limited", category: "marketing fine art print", sub_category: "", image_url: "https://cdn.shopify.com/s/files/1/1108/5520/products/TLOU-ColorFinals_0001_Var.jpg?v=1510692302", artist_id: kevin_t.id)
+Print.create(title: "Pixies", year: 2018, edition_size: "limited", category: "concert poster", sub_category: "", image_url: "https://cdn.shopify.com/s/files/1/1108/5520/products/PIXIES_0000_-REG.jpg?v=1533015855", artist_id: kevin_t.id)
 
-Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
-Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
+jay_r = Artist.create(name: "Jay Ryan", location: "Evanston, IL", bio: "Jay established a reputation for hand-drawn text and a computer-free design process. His prints aim to attain the same goals as other concert posters, but using raccoons and toasters instead of skulls, breasts and hot rods.", artist_photo: "https://www.chicagomag.com/wp-content/archive/images/2009/December2009/C1209_Jay_Ryan.jpg", website_url: "https://thebirdmachine.com/")
+Print.create(title: "Future of The Left", year: 2009, edition_size: "255", category: "concert poster", sub_category: "", image_url: "https://i.ebayimg.com/images/g/FRsAAOSwUylgTSku/s-l1600.jpg", artist_id: jay_r.id)
+Print.create(title: "All The Wrong Places", year: 2019, edition_size: "", category: "fine art print", sub_category: "", image_url: "https://sebastianfoster.com/wp-content/uploads/2021/10/jay_all_wrong_places_detail.jpg", artist_id: jay_r.id)
+Print.create(title: "Diptych of Humorous Fucking Exasperation", year: 2021, edition_size: "200", category: "social commentary", sub_category: "", image_url: "https://cdn.shopify.com/s/files/1/0155/4979/products/image_ace457df-3354-40a8-be30-e405197f35c3_1024x1024.jpg?v=1630690914", artist_id: jay_r.id)
+
+lara = Artist.create(name: "Lara", location: "Berlin, Germany", bio: "A strong female artist creating beautiful and dark illustrations and posters for the dark side of music.", artist_photo: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/314487584_561895479270447_9215287760466824718_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=a_lp57MBkuIAX_5W7y1&_nc_ht=scontent-ord5-2.xx&oh=00_AfDcDa-h7baIk15oysWWdg40MZlPUKyOhP-RRxVV66ttVg&oe=639377B5", website_url: "https://www.subterraneanprints.com/")
+Print.create(title: "Perpetual Owl", year: 2022, edition_size: "limited", category: "concert poster", sub_category: "", image_url: "https://usercontent.one/wp/www.subterraneanprints.com/wp-content/uploads/2022/10/artprintPERPETUALOWL_04.jpg?media=1668847581", artist_id: lara.id)
+Print.create(title: "Afsky", year: 2022, edition_size: "limited", category: "concert poster", sub_category: "", image_url: "https://usercontent.one/wp/www.subterraneanprints.com/wp-content/uploads/2022/10/posterAFSKY_01.jpg?media=1668847581", artist_id: lara.id)
+Print.create(title: "Toxic Thoughts", year: 2022, edition_size: "limited", category: "fine art print", sub_category: "", image_url: "https://usercontent.one/wp/www.subterraneanprints.com/wp-content/uploads/2022/07/artprintTOXIC.jpg?media=1668847581", artist_id: lara.id)
+
+tlfop = Artist.create(name: "Little Friends of Printmaking", location: "Los Angeles, CA", bio: "The Little Friends of Printmaking, otherwise known as JW & Melissa Buchanan, the award-winning illustrators and renowned silkscreen artists (and poster designers and art directors and writers and printmaking lecturers and etc.) based in Los Angeles, CA.", artist_photo: "https://jenhewett.com/wp-content/uploads/2017/03/LFOP_PressPhotoHighRes_square.jpg", website_url: "https://thelittlefriendsofprintmaking.com/oops/")
+Print.create(title: "Silkscreen Is Easy!!!", year: 2021, edition_size: "monoprint", category: "poster print", sub_category: "", image_url: "https://thelittlefriendsofprintmaking.com/oops/images/easy.gif", artist_id: tlfop.id)
+Print.create(title: "Print Shop", year: 2020, edition_size: "open", category: "poster print", sub_category: "", image_url: "https://thelittlefriendsofprintmaking.com/oops/images/printshop.gif", artist_id: tlfop.id)
+Print.create(title: "Love Records", year: 2021, edition_size: "open", category: "marketing poster print", sub_category: "", image_url: "https://thelittlefriendsofprintmaking.com/oops/images/love_records.gif", artist_id: tlfop.id)
+
+# Artist.create(name: "", location: "", bio: "", artist_photo: "", website_url: "")
+# Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
+# Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
+# Print.create(title: "", year: , edition_size: "", category: "", sub_category: "", image_url: "", artist_id: )
 
 puts "✅ Done seeding!"

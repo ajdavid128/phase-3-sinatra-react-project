53a4ad145317da83c4d01279102a1ab7453aa19e
Aaron David <ajdavid128@gmail.com>
2023-02-24 13:59:57 -0600

    changed error message in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Whoops, wrong route on the back-end!" }.to_json
   end
 
 end
